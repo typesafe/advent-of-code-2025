6734d2c3f064184035568d494231ff8bceacd855
Gino Heyman <gino.heyman@gmail.com>
2025-12-02 20:19:07 +0100

    day 2, part 2

--- src/day2.rs
+++ src/day2.rs
@@ -22,6 +22,30 @@ pub fn part1() -> u64 {
     return result;
 }
 
+pub fn part2() -> u64 {
+    let input = std::fs::read_to_string("./day2/input.txt").unwrap();
+
+    let mut result = 0;
+
+    for range in input.split(',') {
+        let (mut number, end) = {
+            let mut splits = range.trim().split('-');
+            let s = u64::from_str_radix(splits.next().unwrap(), 10).unwrap();
+            let e = u64::from_str_radix(splits.next().unwrap(), 10).unwrap();
+            (s, e)
+        };
+
+        while number <= end {
+            if consists_of_repeated_digits2(number) {
+                result += number;
+            }
+            number += 1;
+        }
+    }
+
+    return result;
+}
+
 fn consists_of_repeated_digits(number: u64) -> bool {
     let s = number.to_string();
 
@@ -32,6 +56,38 @@ fn consists_of_repeated_digits(number: u64) -> bool {
     return s[0..s.len() / 2] == s[s.len() / 2..s.len()];
 }
 
+fn consists_of_repeated_digits2(number: u64) -> bool {
+    let s = number.to_string();
+
+    let mut repetitions = 2;
+    let mut result = false;
+
+    while repetitions <= s.len() {
+        if s.len() % repetitions == 0 {
+            let len = s.len() / repetitions;
+            let first = s[0..len].to_string();
+            let mut match_all = true;
+
+            for i in 1..repetitions {
+                if s[i * len..(i + 1) * len] != first {
+                    match_all = false;
+                    break;
+                }
+            }
+
+            if match_all {
+                result = true;
+                break;
+            }
+        } else {
+            repetitions += 1;
+            continue;
+        }
+    }
+
+    return result;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -39,5 +95,10 @@ mod tests {
     #[test]
     fn test_day2_part1() {
         assert_eq!(part1(), 30323879646);
+    }
+
+    #[test]
+    fn test_day2_part2() {
+        assert_eq!(part2(), 43872163557);
     }
 }
