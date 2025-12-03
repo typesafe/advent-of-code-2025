7e45f076531b67a1399fa35499b4f01c96d96c63
Gino Heyman <gino.heyman@gmail.com>
2025-12-03 18:13:26 +0100

    day 3, part 2

--- src/day3.rs
+++ src/day3.rs
@@ -1,46 +1,71 @@
-use std::string;
-
 pub fn part1() -> u64 {
     let input = std::fs::read_to_string("./day3/input.txt").unwrap();
 
     let mut result: u64 = 0;
 
     for bank in input.lines() {
-        result += find_largest_joltage(bank) as u64;
+        result += find_largest_joltage::<2>(bank) as u64;
     }
 
     return result;
 }
 
-fn find_largest_joltage(bank: &str) -> u8 {
+fn find_largest_joltage<const N: usize>(bank: &str) -> u64 {
     let mut it = bank.chars();
+    let mut digits = [0u8; N];
 
-    let mut digits: (u8, u8) = (
-        it.next().unwrap().to_digit(10).unwrap() as u8,
-        it.next().unwrap().to_digit(10).unwrap() as u8,
-    );
-
-    for c in it {
-        let current = digits.0 * 10 + digits.1;
-        let digit = c.to_digit(10).unwrap() as u8;
-
-        let last_digit = digits.1;
-
-        if digits.0 * 10 + digit > current {
-            digits.1 = digit;
-        }
-
-        if last_digit * 10 + digit > current {
-            digits.0 = last_digit;
-            digits.1 = digit;
+    for i in 0..N {
+        if let Some(c) = it.next() {
+            digits[i] = c.to_digit(10).unwrap() as u8;
         }
     }
 
-    return digits.0 * 10 + digits.1;
+    for c in it {
+        let mut largest_joltage = get_value(&digits);
+        let mut copy = digits.clone();
+        let mut largest = digits;
+        let digit = c.to_digit(10).unwrap() as u8;
+
+        for i in 0..N {
+            for j in 0..N - 1 {
+                if j < i {
+                    copy[j] = digits[j];
+                } else {
+                    copy[j] = digits[j + 1];
+                }
+            }
+            copy[N - 1] = digit;
+
+            let new_joltage: u64 = get_value(&copy);
+            if new_joltage > largest_joltage {
+                largest = copy.clone();
+                largest_joltage = new_joltage;
+            }
+        }
+        digits = largest;
+    }
+
+    return get_value(&digits);
+}
+
+fn get_value<const N: usize>(digits: &[u8; N]) -> u64 {
+    let mut value: u64 = 0;
+    for i in 0..N {
+        value = value * 10 + digits[i] as u64;
+    }
+    return value;
 }
 
 pub fn part2() -> u64 {
-    return 0;
+    let input = std::fs::read_to_string("./day3/input.txt").unwrap();
+
+    let mut result: u64 = 0;
+
+    for bank in input.lines() {
+        result += find_largest_joltage::<12>(bank) as u64;
+    }
+
+    return result;
 }
 
 #[cfg(test)]
@@ -54,6 +79,6 @@ mod tests {
 
     #[test]
     fn test_day3_part2() {
-        assert_eq!(part2(), 0);
+        assert_eq!(part2(), 173065202451341);
     }
 }
